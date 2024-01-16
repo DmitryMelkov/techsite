5b5fe967703a82416c424eb46a4acce2df74b9ad
unknown <C:\Users\MaximSmerdov\AppData\Roaming\The Bat!>
2024-01-16 16:07:54 +0500

    .add

--- js/btnscript.js
+++ js/btnscript.js
@@ -3,16 +3,16 @@ const content = document.querySelector('.content');
 const btnBack = document.querySelector('.main__btn-back');
 const btnLibrClick = document.querySelector('.main__btn-libr');
 const btnContainer = document.querySelector('.main__btn-container');
-const content_intro = document.querySelector('.content__intro');
 const content_libr = document.querySelector('.content__libr');
 const library = document.querySelector('.library');
+const btnhelp = document.querySelector('help__tooltip-btn')
+const helpTooltip = document.querySelector('help__tooltip-text')
 
 btnTechClick.addEventListener('click', () => {
   content.classList.add('active');
   btnTechClick.classList.add('active');
   btnLibrClick.classList.add('active');
   btnContainer.classList.add('active');
-  content_intro.classList.remove('active');
 });
 
 btnLibrClick.addEventListener('click', () => {
@@ -21,13 +21,16 @@ btnLibrClick.addEventListener('click', () => {
   btnContainer.classList.add('active');
   content_libr.classList.add('active');
   library.classList.add('active');
-})
+});
 
 btnBack.addEventListener('click', () => {
   btnTechClick.classList.remove('active');
   content.classList.remove('active');
   btnLibrClick.classList.remove('active');
   btnContainer.classList.remove('active');
-  content_intro.classList.add('active');
   content_libr.classList.remove('active');
+})
+
+btnhelp.addEventListener('mouseenter', () => {
+  helpTooltip.classList.add('active')
 })
